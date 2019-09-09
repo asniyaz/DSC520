2a668715d62b63dd8452c05ef6e55eaa965788f1
asniyaz <51541928+asniyaz@users.noreply.github.com>
2019-09-09 12:34:41 -0400

    Delete week2AhmedNiyaz.R

--- week2AhmedNiyaz.R
+++
@@ -1,9 +0,0 @@
-# Week 2 Assignment
-# Statistics for Data science
-# Ahmed Niyaz
-
-#1 What are the observational units in this study?
-# The observational units are the students enrolled in each of the sections 
-
-#2 Identify the variables mentioned in the narrative paragraph and determine which are categorical and quantitative?
-# Section - Categorical / Score - Quantitative 
